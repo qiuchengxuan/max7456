ddc784d90d054ae4954d013328e88a94b0417a8c
qiuchengxuan <qiuchengxuan@gmail.com>
2020-05-17 18:54:25 +0800

    fix issues

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,26 @@
 #![no_std]
 extern crate peripheral_register;
-use core::cell::Cell;
 
 use peripheral_register::Register;
 pub mod registers;
+use embedded_hal::blocking::delay::{DelayMs, DelayUs};
+use embedded_hal::blocking::spi::{Transfer, Write};
+use embedded_hal::spi::{Mode, Phase, Polarity};
 use registers::*;
 
 pub const CHAR_DATA_SIZE: usize = 54;
-pub const ROW: usize = 30;
-pub const COLUMN: usize = 16;
+pub const ROW: usize = 16;
+pub const COLUMN: usize = 30;
 
 pub type Screen = [[u8; COLUMN as usize]; ROW as usize];
 
-pub trait Spi {
-    type Error;
-    fn write(&self, bytes: &[u8]) -> Result<(), Self::Error>;
-    fn write_read(&self, input: &[u8], output: &mut [u8]) -> Result<(), Self::Error>;
-}
+pub const SPI_MODE: Mode = Mode {
+    polarity: Polarity::IdleHigh,
+    phase: Phase::CaptureOnSecondTransition,
+};
 
-pub trait DelayUs {
-    fn delay_us(&self, us: usize);
-}
-
-pub struct MAX7456<'a, S, D> {
-    bus: Cell<&'a S>,
-    delay: Cell<&'a D>,
+pub struct MAX7456<BUS> {
+    bus: BUS,
 }
 
 pub struct Attributes {
@@ -43,50 +39,66 @@ impl Default for Attributes {
     }
 }
 
-impl<'a, S: Spi, D: DelayUs> MAX7456<'a, S, D> {
-    pub fn new(bus: &'a S, delay: &'a D) -> Self {
-        MAX7456 {
-            bus: Cell::new(bus),
-            delay: Cell::new(delay),
-        }
+impl<'a, E, BUS: Write<u8, Error = E> + Transfer<u8, Error = E>> MAX7456<BUS> {
+    pub fn new(bus: BUS) -> Self {
+        MAX7456 { bus }
     }
 
-    fn load<T: From<u8>>(&self, reg: Registers) -> Result<T, S::Error> {
-        let bus = self.bus.get();
+    fn load<T: From<u8>>(&mut self, reg: Registers) -> Result<T, E> {
         let mut value = [0u8; 1];
-        bus.write_read(&[reg.read_address()], &mut value)?;
+        self.bus.write(&[reg.read_address()])?;
+        self.bus.transfer(&mut value)?;
         Ok(T::from(value[0]))
     }
 
-    pub fn disable_display(&self, disable: bool) -> Result<(), S::Error> {
-        let mut video_mode_0: Register<u8, VideoMode0> = self.load(Registers::VideoMode0)?;
-        video_mode_0.set(VideoMode0::EnableDisplay, disable as u8);
-        let bus = self.bus.get();
-        bus.write(&[Registers::VideoMode0 as u8, video_mode_0.value])
-    }
-
-    pub fn set_standard(&self, standard: Standard) -> Result<(), S::Error> {
-        let mut video_mode_0: Register<u8, VideoMode0> = self.load(Registers::VideoMode0)?;
-        video_mode_0.set(VideoMode0::Standard, standard as u8);
-        let bus = self.bus.get();
-        bus.write(&[Registers::VideoMode0 as u8, video_mode_0.value])
-    }
-
-    pub fn clear_display(&self) -> Result<(), S::Error> {
-        let mut dmm: Register<u8, DisplayMemoryMode> = self.load(Registers::DisplayMemoryMode)?;
-        dmm.set(DisplayMemoryMode::Clear, 1);
-        let bus = self.bus.get();
-        bus.write(&[Registers::DisplayMemoryMode as u8, dmm.value])?;
-        let delay = self.delay.get();
-        delay.delay_us(20);
-        while dmm.get(DisplayMemoryMode::Clear) > 0 {
-            dmm = self.load(Registers::DisplayMemoryMode)?;
+    pub fn reset<D: DelayMs<u8>>(&mut self, delay: &mut D) -> Result<(), E> {
+        let mut video_mode_0: Register<u8, VideoMode0> = Register::of(VideoMode0::SoftwareReset, 1);
+        self.bus
+            .write(&[Registers::VideoMode0 as u8, video_mode_0.value])?;
+        delay.delay_ms(50u8);
+        while video_mode_0.get(VideoMode0::SoftwareReset) > 0 {
+            video_mode_0 = self.load(Registers::VideoMode0)?;
+            delay.delay_ms(1u8);
         }
         Ok(())
     }
 
-    pub fn write_char(&self, index: u8, data: &[u8; CHAR_DATA_SIZE]) -> Result<(), S::Error> {
-        self.disable_display(true)?;
+    pub fn enable_display(&mut self, enable: bool) -> Result<(), E> {
+        let mut video_mode_0: Register<u8, VideoMode0> = self.load(Registers::VideoMode0)?;
+        video_mode_0.set(VideoMode0::EnableDisplay, enable as u8);
+        self.bus
+            .write(&[Registers::VideoMode0 as u8, video_mode_0.value])
+    }
+
+    pub fn set_standard(&mut self, standard: Standard) -> Result<(), E> {
+        let mut video_mode_0: Register<u8, VideoMode0> = self.load(Registers::VideoMode0)?;
+        video_mode_0.set(VideoMode0::Standard, standard as u8);
+        self.bus
+            .write(&[Registers::VideoMode0 as u8, video_mode_0.value])
+    }
+
+    pub fn clear_display<D: DelayUs<u8>>(&mut self, delay: &mut D) -> Result<bool, E> {
+        let mut dmm: Register<u8, DisplayMemoryMode> = self.load(Registers::DisplayMemoryMode)?;
+        dmm.set(DisplayMemoryMode::Clear, 1);
+        self.bus
+            .write(&[Registers::DisplayMemoryMode as u8, dmm.value])?;
+        for _ in 0..5 {
+            delay.delay_us(20);
+            dmm = self.load(Registers::DisplayMemoryMode)?;
+            if dmm.get(DisplayMemoryMode::Clear) == 0 {
+                return Ok(true);
+            }
+        }
+        Ok(false)
+    }
+
+    pub fn display_char<D: DelayMs<u8>>(
+        &mut self,
+        index: u8,
+        data: &[u8; CHAR_DATA_SIZE],
+        delay: &mut D,
+    ) -> Result<(), E> {
+        self.enable_display(false)?;
         let mut operations = [0u8; 2 + CHAR_DATA_SIZE * 4 + 2];
         operations[0] = Registers::CharacterMemoryAddressHigh as u8;
         operations[1] = index;
@@ -99,29 +111,26 @@ impl<'a, S: Spi, D: DelayUs> MAX7456<'a, S, D> {
         }
         operations[2 + 54 * 3] = CharacterMemoryMode::WriteToNVM as u8;
         operations[2 + 54 * 3 + 1] = Registers::CharacterMemoryMode as u8;
-        let bus = self.bus.get();
-        bus.write(&operations)?;
-        let delay = self.delay.get();
-        delay.delay_us(12 * 1000);
+        self.bus.write(&operations)?;
+        delay.delay_ms(12);
         loop {
-            let status = Register::<u8, Status>::new(0);
-            bus.write_read(&[Registers::Status.read_address()], status.into())?;
+            let status: Register<u8, Status> = self.load(Registers::Status)?;
             let status = status.get(Status::CharacterMemoryStatus);
             if status == CharacterMemoryStatus::Available as u8 {
                 break;
             }
         }
-        self.disable_display(false)
+        self.enable_display(true)
     }
 
     pub fn display_line(
-        &self,
+        &mut self,
         row: u8,
         column: u8,
         data: &[u8],
         attributes: Attributes,
-    ) -> Result<usize, S::Error> {
-        let mut operations = [0u8; 6 + ROW * 2 + 1];
+    ) -> Result<usize, E> {
+        let mut operations = [0u8; 6 + ROW * 2 + 2];
         operations[0] = Registers::DisplayMemoryMode as u8;
         let mut dmm = Register::<u8, DisplayMemoryMode>::new(0);
         dmm.set(
@@ -143,7 +152,11 @@ impl<'a, S: Spi, D: DelayUs> MAX7456<'a, S, D> {
         operations[4] = Registers::DisplayMemoryAddressLow as u8;
         operations[5] = address as u8;
         let mut offset = 6;
-        let slice = if data.len() > ROW { data } else { &data[..ROW] };
+        let slice = if data.len() <= ROW {
+            data
+        } else {
+            &data[..ROW]
+        };
         let mut ff_checker = false;
         for &byte in slice.iter() {
             operations[offset] = Registers::DisplayMemoryDataIn as u8;
@@ -154,18 +167,13 @@ impl<'a, S: Spi, D: DelayUs> MAX7456<'a, S, D> {
         if ff_checker == true {
             return Ok(0);
         }
-        operations[offset] = 0xFF;
-        let bus = self.bus.get();
-        bus.write(&operations)?;
+        operations[offset] = Registers::DisplayMemoryDataIn as u8;
+        operations[offset + 1] = 0xFF;
+        self.bus.write(&operations)?;
         Ok(slice.len())
     }
 
-    pub fn display_not_null(
-        &self,
-        screen: &Screen,
-        attributes: Attributes,
-    ) -> Result<(), S::Error> {
-        let bus = self.bus.get();
+    pub fn display_not_null(&mut self, screen: &Screen, attributes: Attributes) -> Result<(), E> {
         let mut operations = [0u8; 2 + COLUMN * 6];
         operations[0] = Registers::DisplayMemoryMode as u8;
         let mut dmm = Register::<u8, DisplayMemoryMode>::new(0);
@@ -196,14 +204,60 @@ impl<'a, S: Spi, D: DelayUs> MAX7456<'a, S, D> {
                 operations[offset + 5] = line[column];
                 offset += 6;
                 if offset >= operations.len() {
-                    bus.write(&operations)?;
+                    self.bus.write(&operations)?;
                     offset = 2;
                 }
             }
         }
         if offset > 2 {
-            bus.write(&operations[..offset])?;
+            self.bus.write(&operations[..offset])?;
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+#[macro_use]
+extern crate std;
+
+#[cfg(test)]
+mod test {
+    use embedded_hal::blocking::spi::{Transfer, Write};
+
+    use super::MAX7456;
+
+    struct Bus<'a>(&'a mut [u8]);
+
+    impl<'a> Write<u8> for Bus<'a> {
+        type Error = &'static str;
+        fn write(&mut self, bytes: &[u8]) -> Result<(), &'static str> {
+            if self.0.len() >= bytes.len() {
+                let len = bytes.len();
+                self.0[..len].copy_from_slice(bytes);
+            } else {
+                let len = self.0.len();
+                self.0.copy_from_slice(&bytes[..len]);
+            }
+            Ok(())
+        }
+    }
+
+    impl<'a> Transfer<u8> for Bus<'a> {
+        type Error = &'static str;
+        fn transfer<'b>(&mut self, bytes: &'b mut [u8]) -> Result<&'b [u8], &'static str> {
+            self.write(bytes)?;
+            Ok(&[])
+        }
+    }
+
+    #[test]
+    fn test_display_line() {
+        let mut buffer = [0u8; 16];
+        let bus = Bus(&mut buffer);
+        let mut max7456 = MAX7456::new(bus);
+        let _result = max7456.display_line(0, 0, b"test", Default::default());
+        let actual = format!("{:x?}", buffer);
+        let expected = "[4, 1, 5, 0, 6, 0, 7, 74, 7, 65, 7, 73, 7, 74, 7, ff]";
+        assert_eq!(expected, actual)
     }
 }
